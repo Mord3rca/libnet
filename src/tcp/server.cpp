30a3a8bb5a3f225c664bd5508fbb583fb83c306c
Mord3rca <morderca@morderca.net>
2018-09-10 11:25:10 +0200

    [ADD] TCP Timeout

--- src/tcp/server.cpp
+++ src/tcp/server.cpp
@@ -13,97 +13,82 @@ bool sock_non_block(int fd) noexcept
   return true;
 }
 
-tcp::Server::Server() : m_sockfd(-1), m_pollfd(-1),
-                        m_run(false), m_maxconn(64),
-                        m_buff(nullptr), m_buffsize(2048)
-{}
-
-tcp::Server::Server(const std::string &ip, const unsigned int port) : Server()
+template<>
+void tcp::Server<int>::OnReceived( void *ptr, const char* buff, ssize_t count)
 {
-  this->bind(ip, port);
+  int fd = *( static_cast<int*>(ptr) );
+  ::write(fd, buff, count);
 }
 
-tcp::Server::~Server()
+template<>
+struct tcp::Server<int>::Worker::container* tcp::Server<int>::Worker::create( int fd )
 {
-  if(m_buff) delete m_buff;
-}
-
-void tcp::Server::bind( const std::string &ip, const unsigned int port)
-{
-  struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port   = htons(port);
-    addr.sin_addr.s_addr = inet_addr(ip.c_str());
-  
-  m_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if( m_sockfd < 0 )
-    throw std::runtime_error("socket(): error");
-  
-  int turnon = 1;
-  setsockopt(m_sockfd, SOL_SOCKET, SO_REUSEADDR, &turnon, sizeof(turnon));
-  
-  if( ::bind(m_sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0 )
-    throw std::runtime_error("bind(): error");
-  
-  if( listen(m_sockfd, m_maxconn) < 0 )
-    throw std::runtime_error("listen(): error");
-  
-  if( !sock_non_block(m_sockfd) )
+  if( !sock_non_block(fd) )
     throw std::runtime_error("sock_non_block(): error");
-}
-
-void tcp::Server::maxConnection( unsigned int conn )
-{
-  if( !m_run ) m_maxconn = conn;
-}
-
-void tcp::Server::buffSize( unsigned int size )
-{
-  if( m_run ) return;
   
-  m_buffsize = size;
-  if( m_buff ) delete m_buff;
+  struct epoll_event event;
+  auto *obj = new tcp::Server<int>::Worker::container(fd, nullptr);
+  event.data.ptr = obj;
+  event.events = EPOLLIN | EPOLLET;
+  if( epoll_ctl(root->m_pollfd, EPOLL_CTL_ADD, fd, &event) == -1 )
+    throw std::runtime_error("epoll_ctl() error");
   
-  m_buff = new char[m_buffsize];
+  m_clients.push_back(obj);
+  
+  return nullptr;
 }
 
-void tcp::Server::start()
+template<>
+void tcp::Server<int>::Worker::close( struct tcp::Server<int>::Worker::container *ptr )
+{
+  epoll_ctl(root->m_pollfd, EPOLL_CTL_DEL, ptr->fd, nullptr);
+  ::close(ptr->fd);
+  
+  auto i = std::find(m_clients.begin(), m_clients.end(), ptr);
+  if( i != m_clients.end() )
+    m_clients.erase(i);
+  
+  delete ptr;
+}
+
+template<>
+void tcp::Server<int>::Worker::loop()
 {
   struct epoll_event *events;
   
   events = new struct epoll_event[64];
   
-  m_pollfd = epoll_create1(0);
-  if( m_pollfd == -1 )
+  root->m_pollfd = epoll_create1(0);
+  if( root->m_pollfd == -1 )
     throw std::runtime_error("epoll_create1(): Error");
   
-  _tcp_create( m_sockfd );
+  create( root->m_sockfd );
   
-  if(!m_buff) this->buffSize(m_buffsize);
+  if(!m_buff) m_buff = new char[root->m_buffsize];
   
-  m_run = true;
-  while(m_run)
+  root->m_run = true; m_last_timeout_check = time(nullptr);
+  while(root->m_run)
   {
-    int n;
-    n = epoll_wait( m_pollfd, events, 64, 500 );
+    int n = epoll_wait( root->m_pollfd, events, 64, 500 );
     for( int i = 0; i < n; i++ )
     {
+      auto *con_ptr = static_cast<struct tcp::Server<int>::Worker::container*>(events[i].data.ptr);
       if( (events[i].events & EPOLLERR) ||
           (events[i].events & EPOLLHUP) ||
           !(events[i].events & EPOLLIN) )
       {
-        OnError(events[i].data.fd, "Polling error: Object pulled without event.");
-        _tcp_close(events[i].data.fd);
+        root->OnError(&con_ptr->fd, "Polling error: Object pulled without event.");
+        close(con_ptr);
         continue;
       }
-      else if( events[i].data.fd == m_sockfd )
+      else if( con_ptr->fd == root->m_sockfd )
       {
         while(true)
         {
           struct sockaddr in_addr; socklen_t in_len = sizeof(struct sockaddr);
           int infd;
           
-          infd = accept( m_sockfd, &in_addr, &in_len );
+          infd = accept( root->m_sockfd, &in_addr, &in_len );
           if( infd == -1 )
           {
             if( (errno == EAGAIN) ||
@@ -113,28 +98,25 @@ void tcp::Server::start()
               throw std::runtime_error("accept() error in event loop.");
           }
           
-          _tcp_create(infd);
+          create(infd);
           
           std::string ip; int port;
-          _gethostinfo(in_addr, ip, port);
+          gethostinfo(in_addr, ip, port);
           
-          OnConnect(infd, ip, port);
+          root->OnConnect(&infd, ip, port);
         }
         continue;
       }
       else if( events[i].events & EPOLLHUP )
       {
-        int fd = events[i].data.fd;
-        {this->OnDisconnect(fd);}
-        _tcp_close(fd);
+        {root->OnDisconnect(&con_ptr->fd);}
+        close(con_ptr);
       }
       else
       {
-        int fd = events[i].data.fd;
-        
         while(true)
         {
-          ssize_t count = read(fd, m_buff, m_buffsize);
+          ssize_t count = read(con_ptr->fd, m_buff, root->m_buffsize);
           
           if( count == -1)
           {
@@ -145,54 +127,21 @@ void tcp::Server::start()
           }
           else if( count == 0 )
           {
-            {OnDisconnect(fd);}
-            _tcp_close(fd);
+            {root->OnDisconnect(&con_ptr->fd);}
+            close(con_ptr);
             break;
           }
           
-          {OnReceived(fd, m_buff, count);}
+          {root->OnReceived(&con_ptr->fd, m_buff, count);}
         }
       }
+      con_ptr->last_event = time(nullptr);
     }
+    _checkTimeout();
   }
   
   delete[] events;
-  close(m_pollfd);
-  close(m_sockfd);
-  m_sockfd = -1;
-}
-
-void tcp::Server::OnReceived(int fd, const char *buff, ssize_t size)
-{
-  write(fd, buff, size);
-}
-
-void tcp::Server::_tcp_create( int fd )
-{
-  if( !sock_non_block(fd) )
-    throw std::runtime_error("sock_non_block(): error");
-  
-  struct epoll_event event;
-  event.data.fd = fd;
-  event.events = EPOLLIN | EPOLLET;
-  if( epoll_ctl(m_pollfd, EPOLL_CTL_ADD, fd, &event) == -1 )
-    throw std::runtime_error("epoll_ctl() error");
-}
-
-void tcp::Server::_tcp_close( int fd )
-{
-  epoll_ctl(m_pollfd, EPOLL_CTL_DEL, fd, nullptr);
-  close(fd);
-}
-
-void tcp::Server::_gethostinfo(struct sockaddr &addr, std::string& ip, int& port)
-{
-  char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
-  getnameinfo(&addr, sizeof(addr),
-              hbuf, sizeof(hbuf),
-              sbuf, sizeof(sbuf),
-              NI_NUMERICHOST | NI_NUMERICSERV);
-  
-  ip = hbuf;
-  port = std::atoi(sbuf);
+  ::close(root->m_pollfd);
+  ::close(root->m_sockfd);
+  root->m_sockfd = -1;
 }
--- src/tcp/socket.cpp
+++ src/tcp/socket.cpp
@@ -6,6 +6,9 @@ tcp::Socket::Socket() : m_fd(-1)
 tcp::Socket::Socket(int fd) : m_fd(fd)
 {}
 
+tcp::Socket::~Socket()
+{}
+
 ssize_t tcp::Socket::write(const char* buff, ssize_t size)
 {
   return (m_fd != -1) ? ::write(m_fd, buff, size) : -1;
